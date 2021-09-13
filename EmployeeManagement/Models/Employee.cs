2614475c2b3025b8bf18992ecd30b6f877477fdb
meena <meena@LAPTOP-R9NM5S5F>
2021-09-13 21:06:52 +0530

    Add project files.

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+     //   [Required(ErrorMessage = "Please provide a value for Name field")]
+        [Required, MaxLength(50,ErrorMessage ="Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name ="Office Email")]
+        [RegularExpression(@"^[a-zA-Z]+[a-zA-Z0-9]+[[a-zA-Z0-9-_.!#$%'*+/=?^]{1,20}@[a-zA-Z0-9]{1,20}.[a-zA-Z]{2,3}$",ErrorMessage ="Invalid email format")]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+
+    }
+}
---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){ Id=1,Name="Mohit",Department=Dept.IT,Email="mohit@meena.com"},
+                new Employee() { Id = 2, Name = "Venkat", Department = Dept.HR, Email = "venkat@cud.com" },
+                new Employee() { Id = 3, Name = "Mukesh", Department = Dept.PayRole, Email = "mukesh@chand.com" },
+               // new Employee() { Id = 4, Name = "Lokesh", Department = Dept.IT, Email = "lokesh@kumar.com" }
+            };
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
